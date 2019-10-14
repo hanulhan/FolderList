1e99e59a74df5c2d8651fa3869a617471c382530
Uli Hansen <ulrich.hansen@acentic.com>
2019-10-14 13:22:23 +0200

    save before holiday

--- src/main/java/com/hanulhan/consoleApp/FolderBean.java
+++ src/main/java/com/hanulhan/consoleApp/FolderBean.java
@@ -19,11 +19,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class FolderBean {
 
-    static private String rootFolder= "/home/uli";
+    private String rootFolder;
     private static final Logger LOGGER = LogManager.getLogger(FolderBean.class);
     private File f;
     
-    public FolderBean() {
+    public FolderBean(String aRootFolder) {
+        rootFolder= aRootFolder;
         f= new File(rootFolder);
     }
 
@@ -41,5 +42,54 @@ public class FolderBean {
         }
         
     }
-    
+
+
+    ser_folder getFolders(Long parentFolder)    {
+        
+        ser_folder myFolder = new ser_folder();
+        ser_file ser_tempFile;
+        File[] files= f.listFiles();
+        if (files != null) {
+            for (File tempFile : files) {
+                if (tempFile.isFile())  {
+                    ser_tempFile= new ser_file(tempFile);
+                    if (myFolder.getFiles() == null)    {
+                        ser_fileList ser_fileList = new ser_fileList();
+                    }
+                    myFolder.setFiles(files);
+                } else {
+                    
+                }
+            }
+        }
+        for 
+        /*
+        for (File folder : getAssetFolders()) {
+            long idparent = 0;
+            long searchParent = 0;
+            if ((parentFolder != null) && (parentFolder > 0)) {
+                searchParent = parentFolder;
+            }
+            if (folder.getAssetFolders() != null) {
+                idparent = folder.getAssetFolders().getId();
+            }
+            if (idparent == searchParent) {
+                ser_folder f = new ser_folder(folder);
+                f.setFolders(getFolders(folder.getId()));
+                r.add(f);
+                for (entityAssetFile afile : folder.getAssetFiles()) {
+                    if (f.getFiles() == null) {
+                        f.setFiles(new ser_fileList());
+                    }
+                    ser_file newFile = new ser_file(afile);
+                    f.getFiles().add(newFile);
+                }
+            }
+
+        }
+        if (r.size() > 0) {
+            return r;
+        }
+        */
+    }
 }
